87c108a50b919c007a06b7cd4f91daf60ee840c9
Goncalves Théau <theau26@gmail.com>
2021-11-22 12:04:42 +0100

    Date diff et interval

--- date/index.php
+++ date/index.php
@@ -8,11 +8,36 @@ $today = new DateTime();
 $birthDay = new DateTime('1996-08-26 8:00');
 
 $anniversaire = DateTime::createFromFormat('d/m/Y', '26/08/1996');
-dump($birthDay);
-dump($anniversaire);
+
+// echo "Je suis né le " . $birthDay->format('d/m/Y') . ' à ' . $birthDay->format('H\hi') . '<br>';
+// echo "Je suis né le " . $birthDay->format('l d F Y') . ' à ' . $birthDay->format('H\hi');
+
+//Ecriture short
+$dateFromTimestamp = (new DateTime())->setTimestamp(time());
+
+$ageDetails = $birthDay->diff($today);
+/** @var DateInterval $ageDetails */
+
+// echo $ageDetails->format('Je suis né depuis %y ans %m mois et %d jours');
+
+//Licence 30 days
 
 
+$licenceSubscriptionDate = new DateTime('2021-11-01'); //Date de la licence
+$licenceDuration = new DateInterval('P1M'); //Validité max de la licence (1 mois)
+$endOfValidity = clone $licenceSubscriptionDate; //On clone l'object pour ne pas modifier la date initiale
+$endOfValidity->add($licenceDuration); // Ajout d'un mois sur la date de la licence
 
 
-echo "Je suis né le " . $birthDay->format('d/m/Y') . ' à ' . $birthDay->format('H\hi') . '<br>';
-echo "Je suis né le " . $birthDay->format('l d F Y') . ' à ' . $birthDay->format('H\hi');
+function isLicenceValid(Datetime $licenceSubscriptionDate): bool
+{
+    // On ajoute 1 mois pour comparer
+    $endOfValidity = (clone $licenceSubscriptionDate)->add(new DateInterval('P1M'));
+
+    // Si $endOfValidity est plus grand que la date d'aujourd'hui, la licence est valide
+    if($endOfValidity > new DateTime()) {
+        return true;
+    }
+
+    return false;
+}
