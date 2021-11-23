99657be93b3744c2b72afc05603e6505bc40c83f
Goncalves Théau <theau26@gmail.com>
2021-11-23 10:40:21 +0100

    Consigne teacher

---
+++ base/Teacher.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Faire héritage de User
+ * Ajouter les attributs suivants :
+ *  - Numéro d'identification national : nationalTeacherId
+ *  - Dans un tableau associatif : les matières que le teacher peut enseigner avec une note de maitrise
+ *  allant de 0 à 5 -> Exemple : PHP => 5 ( lessonSubjects)
+ *  - Méthode permettant d'obtenir la moyenne de maitrise du teacher -> getAvgLessonCapacity()
+ *  - Premier étudiant formé -> Relation avec student -> firstStudent
+ */
+class Teacher
+{
+
+}
