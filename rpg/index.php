8787d2ba65e65456072a64ce468ff90271165acb
Goncalves Théau <theau26@gmail.com>
2021-11-24 11:47:29 +0100

    Message style + index base html

--- rpg/index.php
+++ rpg/index.php
@@ -1,12 +1,25 @@
 <?php
-
 include '../functions.php';
 include 'App/Hero/Hero.php';
 include 'App/Hero/Warrior.php';
 include 'App/Game/Message.php';
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RPG game</title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+
+
+<?php
 
 Message::startGame();
-
 
 
 $doigbynique = new Warrior('Doigbynique', 'Tu es capable');
@@ -18,4 +31,5 @@ dump($doigbynique);
 
 
 ?>
-
+</body>
+</html>
