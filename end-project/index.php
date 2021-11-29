deb1cf62d8a40145b8ba76c2905fba60554c7456
Goncalves Théau <theau26@gmail.com>
2021-11-29 14:20:45 +0100

    Fix 404 condition

--- end-project/index.php
+++ end-project/index.php
@@ -4,13 +4,13 @@ require 'vendor/autoload.php';
 
 if(empty($_GET['page'])) {
     (new \App\Controller\HomeController())();
-} elseif (!empty($_GET) && !empty($_GET['page'])) {
+} else {
     if($_GET['page'] == 'mentions-legales') {
         (new \App\Controller\LegalNoticeController())();
     } else {
         (new \App\Controller\NotFoundController())();
     }
-} 
+}
 
 
 
