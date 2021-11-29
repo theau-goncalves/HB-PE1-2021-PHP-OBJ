faae1c6c058df8a31a5ad36c4ffc50b7ff24080d
Goncalves Théau <theau26@gmail.com>
2021-11-29 12:42:58 +0100

    Route fake

--- end-project/index.php
+++ end-project/index.php
@@ -2,4 +2,13 @@
 
 require 'vendor/autoload.php';
 
-(new \App\Controller\HomeController())();
+if(empty($_GET['page'])) {
+    (new \App\Controller\HomeController())();
+}
+
+if(!empty($_GET)) {
+    if($_GET['page'] == 'mentions-legales') {
+        (new \App\Controller\HomeController())();
+    }
+}
+
